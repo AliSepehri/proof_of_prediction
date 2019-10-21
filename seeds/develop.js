0158d5a93893674812e36b6d5e3d7b20c68fbcf8
AliSepehri <ali.sepehri.kh@gmail.com>
2019-10-21 22:38:41 +0330

    Create a sample seed

---
+++ seeds/develop.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose'),
+  models = require('../api/models'),
+  userModel = mongoose.model('Users'),
+  predictionModel = mongoose.model('Predictions');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/proofOfPrediction_db');
+
+(async function (){
+  var john = new userModel({
+    email: 'john@example.com',
+    fullname: 'John Doe',
+    createdAt: '2019-09-09'
+  });
+  await john.save()
+
+  var prediction1 = new predictionModel({
+    hash: 'e5734682839e13c91a75172ef859dbbf',
+    createdAt: '20190-10-09',
+    user: john._id
+  })
+  await prediction1.save();
+
+  john.predictions.push(prediction1);
+  await john.save();
+})();
