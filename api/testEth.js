8eadc49b37dac12d995582519004c432bf406253
AliSepehri <ali.sepehri.kh@gmail.com>
2020-01-03 01:40:38 +0330

    Add Event to smart-contract and fetch its arguments after executing transaction

--- api/testEth.js
+++ api/testEth.js
@@ -1,17 +1,28 @@
 (async function() {
   const Web3 = require('web3');
-  const provider = new Web3.providers.HttpProvider("http://localhost:8545");
+  const web3 = new Web3("http://localhost:8545");
   const contract = require("@truffle/contract");
 
   const path = require('path');
   const SimpleStorageJson  = require(path.join(__dirname, 'truffle/build/contracts/SimpleStorage.json'));
   const SimpleStorage = contract(SimpleStorageJson);
-  SimpleStorage.setProvider(provider);
+  SimpleStorage.setProvider(web3._provider);
 
-  let simpleStorageInstance = await SimpleStorage.deployed()
+  let simpleStorageInstance = await SimpleStorage.deployed();
 
-  await simpleStorageInstance.set(10, { from: "0x0223e2b58030Cb7c66AD2867d43160FAdAE3D510" });
+  // Call "set" function of smart-contract
+  let result = await simpleStorageInstance.set(9, { from: "0x0223e2b58030Cb7c66AD2867d43160FAdAE3D510" });
+  console.log("Value = ", result.logs[0].args._value.toNumber());
+  console.log("Setter Address = ", result.logs[0].args._setter);
+  console.log("Gas Price = ", result.receipt.gasUsed);
 
-  let storedValue = await simpleStorageInstance.get();
-  console.log("======>", storedValue.toNumber());
+  // Call "get" function of smart-contract
+  let storedValue = await simpleStorageInstance.get({ from: "0x1bA2cf18C0D49F15A8a3E72cB73b0BB6d0997526" });
+  console.log("Stored Value = ", storedValue.toNumber());
+
+  // Fetch transaction's logs by transaction ID
+  let tx = await web3.eth.getTransactionReceipt(result.tx);
+  let logs = SimpleStorage.decodeLogs(tx.logs);
+  console.log("Value = ", logs[0].args._value.toNumber());
+  console.log("Setter Address = ", logs[0].args._setter);
 })();
--- api/truffle/contracts/SimpleStorage.sol
+++ api/truffle/contracts/SimpleStorage.sol
@@ -2,9 +2,13 @@ pragma solidity >=0.4.0 <0.6.0;
 contract SimpleStorage {
    uint storedData;
 
+   event SetData(address indexed _setter, uint _value);
+
    function set(uint x) public {
       storedData = x;
+      emit SetData(msg.sender, x);
    }
+
    function get() public view returns (uint) {
       return storedData;
    }
