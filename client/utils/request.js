bc1c335e13cf9e2e9f319478c497bb830fd16e85
AliSepehri <ali.sepehri.kh@gmail.com>
2020-12-11 23:26:19 +0330

    Dockerize "client" app

--- client/utils/request.js
+++ client/utils/request.js
@@ -3,17 +3,19 @@ import Cookies from "js-cookie";
 
 export default (ctx = null) => {
   let token;
+  let baseURL;
 
   if(ctx && ctx.req){
     const cookie = ctx.req.headers.cookie
-    if (token)
-      token = cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    if (token) token = cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    baseURL = process.env.BASE_URL
   }else{
     token = Cookies.get("token");
+    baseURL = process.env.NEXT_PUBLIC_BASE_URL
   }
 
   const request = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: baseURL,
     headers: { Authorization: `Bearer ${token}` }
   });
 
