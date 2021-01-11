cbae1c8fe4eec968e69153c371a61cadf62736ed
AliSepehri <ali.sepehri.kh@gmail.com>
2021-01-11 22:42:25 +0330

    Return 200 on root URL for api app

---
+++ api/app/controllers/homeController.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.index = function(req, res) { 
+  res.json({message: 'APIs are working!'});
+};
--- api/app/routes/index.js
+++ api/app/routes/index.js
@@ -1,7 +1,12 @@
 var predictionRoutes = require('./predictionRoutes'),
   sessionRoutes = require('./sessionRoutes');
 
+var homeController = require('../controllers/homeController');
+
 module.exports = function(app) {
+  app.route('/')
+  .get(homeController.index);
+
   predictionRoutes(app);
   sessionRoutes(app);
 }
